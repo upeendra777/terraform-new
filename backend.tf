3aff4541b63f6d2ba550bb51d5878372c464c820
Sainath Parimi <sainathparimi@gmail.com>
2025-08-08 12:02:58 +0530

    Initial commit to new repo

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "gcs" {
-    bucket = "afroz-terraform"  # GCS bucket name
+    bucket = "sainath-terraform"  # GCS bucket name
     prefix = "terraform/state"              # Path within the bucket (e.g., a folder structure)
   }
 }
